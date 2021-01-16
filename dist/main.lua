a0d67db208ba61866ba6d71138de94fa87c66ec7
Henning Berge <henning@styreportalen.no>
2021-01-16 23:27:24 +0100

    nuke turtle

--- dist/main.lua
+++ dist/main.lua
@@ -1,14 +1,16 @@
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 5,["5"] = 5,["6"] = 10,["7"] = 10,["8"] = 10,["9"] = 14,["10"] = 14,["11"] = 14,["12"] = 14,["13"] = 14});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 5,["5"] = 5,["6"] = 10,["7"] = 10,["8"] = 10,["9"] = 14,["10"] = 14,["11"] = 14,["12"] = 14,["13"] = 14,["14"] = 14,["15"] = 14});
 local ____exports = {}
 local ____turtle = require("world.turtle")
 local Turtle = ____turtle.Turtle
 print(
     Turtle:getDiskDrive()
 )
-Turtle:configFromDrive(
-    ("/" .. tostring(
-        Turtle:getDiskDrive()
-    )) .. "/location.json"
+print(
+    Turtle:configFromDrive(
+        ("/" .. tostring(
+            Turtle:getDiskDrive()
+        )) .. "/location.json"
+    )
 )
 return ____exports
