7edd4c614f43c9b54930f9d1078ee96b59c550f1
Henning Berge <henning@styreportalen.no>
2021-01-17 02:10:45 +0100

    nuke coordinate in dir

--- dist/client.lua
+++ dist/client.lua
@@ -1,11 +1,26 @@
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 3,["5"] = 3,["6"] = 3,["8"] = 9,["9"] = 11,["10"] = 8});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 3,["5"] = 3,["6"] = 3,["7"] = 11,["8"] = 15,["9"] = 15,["10"] = 15,["11"] = 16,["12"] = 17,["13"] = 17,["14"] = 17,["15"] = 17,["17"] = 18,["18"] = 19,["19"] = 20,["22"] = 24,["23"] = 25,["25"] = 11});
 local ____exports = {}
 ____exports.Client = __TS__Class()
 local Client = ____exports.Client
 Client.name = "Client"
-function Client.prototype.____constructor(self)
-    print("find data")
-    print("IM Done!")
+function Client.prototype.____constructor(self, configPath)
+    local filehandle = ({
+        fs.open(configPath, "r")
+    })[1]
+    if filehandle then
+        local raw = {
+            textutils.unserializeJSON(
+                filehandle:readAll()
+            )
+        }
+        if raw[1] then
+            local data = raw[1]
+            print(data)
+        end
+    end
+    if not filehandle then
+        print("unable to load files")
+    end
 end
 return ____exports
--- dist/main.lua
+++ dist/main.lua
@@ -1,16 +1,21 @@
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 5,["5"] = 5,["6"] = 10,["7"] = 10,["8"] = 10,["9"] = 14,["10"] = 14,["11"] = 14,["12"] = 14,["13"] = 14,["14"] = 14,["15"] = 14});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 1,["5"] = 1,["6"] = 2,["7"] = 2,["8"] = 4,["9"] = 6,["10"] = 7,["11"] = 8,["12"] = 8,["13"] = 8,["14"] = 8,["15"] = 8,["16"] = 8,["17"] = 8,["20"] = 12});
 local ____exports = {}
 local ____turtle = require("world.turtle")
 local Turtle = ____turtle.Turtle
-print(
-    Turtle:getDiskDrive()
-)
-print(
-    Turtle:configFromDrive(
-        ("/" .. tostring(
-            Turtle:getDiskDrive()
-        )) .. "/location.json"
-    )
-)
+local ____client = require("client")
+local Client = ____client.Client
+local dataJson = Turtle:findData()
+if dataJson then
+    if __TS__StringStartsWith(dataJson, "/disk") then
+        print(
+            Turtle:configFromDrive(
+                ("/" .. tostring(
+                    Turtle:getDiskDrive()
+                )) .. "/location.json"
+            )
+        )
+    end
+end
+local c = __TS__New(Client, dataJson)
 return ____exports
