f8274312474010b1a1ac3f5e742958cff0107b30
Henning Berge <henning@styreportalen.no>
2021-01-17 14:52:18 +0100

    add data

--- dist/client.lua
+++ dist/client.lua
@@ -1,5 +1,5 @@
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 1,["5"] = 1,["6"] = 2,["7"] = 2,["8"] = 4,["9"] = 4,["10"] = 4,["11"] = 10,["12"] = 11,["13"] = 12,["14"] = 13,["15"] = 14,["16"] = 10,["17"] = 17,["18"] = 18,["19"] = 19,["20"] = 19,["22"] = 20,["24"] = 17,["25"] = 24,["26"] = 25,["27"] = 26,["28"] = 26,["29"] = 26,["30"] = 27,["31"] = 27,["33"] = 28,["34"] = 29,["36"] = 31,["38"] = 24});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 1,["5"] = 1,["6"] = 2,["7"] = 2,["8"] = 4,["9"] = 4,["10"] = 4,["11"] = 10,["12"] = 11,["13"] = 12,["14"] = 13,["15"] = 14,["16"] = 10,["17"] = 17,["18"] = 18,["19"] = 19,["21"] = 19,["22"] = 19,["23"] = 19,["25"] = 17,["26"] = 23,["27"] = 24,["28"] = 25,["29"] = 25,["30"] = 25,["31"] = 26,["32"] = 26,["34"] = 27,["35"] = 28,["37"] = 30,["39"] = 23});
 local ____exports = {}
 local ____turtle = require("world.turtle")
 local Turtle = ____turtle.Turtle
@@ -16,10 +16,11 @@ function Client.prototype.____constructor(self, configPath)
 end
 function Client.prototype.listener(self)
     while true do
-        local data = {
-            self.websocket:receive()
-        }
-        print(data)
+        print(
+            {
+                self.websocket:receive()
+            }
+        )
     end
 end
 function Client.prototype.connectSocket(self)
