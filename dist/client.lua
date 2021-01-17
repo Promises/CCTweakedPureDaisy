b21404d5f8d3de96561637630808aba4eb121259
Henning Berge <henning@styreportalen.no>
2021-01-17 19:55:45 +0100

    remove source traceback

--- dist/client.lua
+++ dist/client.lua
@@ -8,17 +8,30 @@ ____exports.Client = __TS__Class()
 local Client = ____exports.Client
 Client.name = "Client"
 function Client.prototype.____constructor(self, configPath)
+    self.running = false
     local data = loadConfigFile(configPath)
     self.websocketAddress = data.server
     self.websocketPort = data.port
     self.localTurtle = __TS__New(Turtle, configPath)
 end
 function Client.prototype.listener(self)
-    print(
-        ({
-            self.websocket.receive()
-        })[1]
-    )
+    self.running = true
+    while self.running do
+        self:handleAction(
+            ({
+                self.websocket.receive()
+            })[1]
+        )
+    end
+end
+function Client.prototype.listenForTerminate(self)
+    local key
+    repeat
+        do
+            local input = os.pullEvent("key")
+            print(input[2])
+        end
+    until not true
 end
 function Client.prototype.connectSocket(self)
     local url = (("ws://" .. tostring(self.websocketAddress)) .. ":") .. tostring(self.websocketPort)
@@ -33,5 +46,16 @@ function Client.prototype.connectSocket(self)
     else
         print(ws[2])
     end
+end
+function Client.prototype.handleAction(self, receiveElement)
+    local ____switch11 = receiveElement
+    goto ____switch11_case_default
+    ::____switch11_case_default::
+    do
+        print("Unknown action:")
+        print(receiveElement)
+        goto ____switch11_end
+    end
+    ::____switch11_end::
 end
 return ____exports
--- dist/main.lua
+++ dist/main.lua
@@ -4,7 +4,7 @@ local ____turtle = require("world.turtle")
 local Turtle = ____turtle.Turtle
 local ____client = require("client")
 local Client = ____client.Client
-print("starting v0.0.1")
+print("starting v0.0.2")
 local dataJson = Turtle:findData()
 if dataJson then
     if __TS__StringStartsWith(dataJson, "/disk") then
@@ -25,11 +25,7 @@ do
             if c.websocket then
                 parallel.waitForAll(
                     function() return c:listener() end,
-                    function()
-                        while true do
-                            turtle.turnLeft()
-                        end
-                    end
+                    function() return c:listenForTerminate() end
                 )
             else
                 print("not connected")
